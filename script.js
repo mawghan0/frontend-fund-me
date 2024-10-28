2f53ce2af76ae1421f0cf073422432e6e13b5806
Muhammad Yusuf Habibie <myh481813@gmail.com>
2024-10-28 11:04:51 +0700

    add: fundFunction, fixAll

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
+import { ethers } from "https://cdnjs.cloudflare.com/ajax/libs/ethers/6.7.0/ethers.min.js";
+import { abi, contractAddress } from "./constants.js";
+
+const connectButton = document.getElementById("connectButton");
+const fundButton = document.getElementById("fundButton");
+
+connectButton.onclick = connect;
+fundButton.onclick = fund;
+
+const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+if (accounts.length !== 0) {
+  connectButton.innerHTML = "Connected";
+}
+
 async function connect() {
   if (typeof window.ethereum !== "undefined") {
     await window.ethereum.request({ method: "eth_requestAccounts" });
     // console.log("metamask connected");
-    document.getElementById("buttonConnect").innerHTML = "Connected";
+    connectButton.innerHTML = "Connected";
   } else {
-    document.getElementById("buttonConnect").innerHTML = "Metamask not installed";
+    alert("Metamask is not installed");
+  }
+}
+
+async function fund() {
+  const provider = new ethers.BrowserProvider(window.ethereum);
+  const signer = await provider.getSigner();
+  const ethAmount = "0.1"
+  if (signer) {
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    try {
+      const transactionResponse = await contract.fund({ value: ethers.parseEther(ethAmount) });
+      const receipt = await transactionResponse.wait();
+      if (receipt) {
+        alert(`Fund Success with tx: ${receipt.hash}`)
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  } else {
+    connect();
   }
 }
